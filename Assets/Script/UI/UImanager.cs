df4e1fe038288ce459442429638d5195b0eca196
Piero-Jeri <piero.jeri@tecsup.edu.pe>
2026-05-27 17:43:59 -0500

    UIManager

--- Assets/Script/UI/UImanager.cs
+++ Assets/Script/UI/UImanager.cs
@@ -1,14 +1,25 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UImanager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Button btn;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    public void test()
+    {
+
+    }
+
+    public void LoadScene(int SceneID)
+    {
+        SceneManager.LoadScene(SceneID);
+    }
+
     void Update()
     {
         
