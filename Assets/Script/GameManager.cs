fffc013d4ffa8b022736c450917579781274d303
Piero-Jeri <piero.jeri@tecsup.edu.pe>
2026-06-03 17:44:37 -0500

    Waypoints

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -2,15 +2,41 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float tiempoMaximo;
+    private float tiempoActual;
+    private bool tiempoActivo = false;
+    private void Update()
     {
-      
+        if (tiempoActivo)
+        {
+            CambiarContador();
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CambiarContador()
     {
-        
+        tiempoActual -= Time.deltaTime;
+
+        if (tiempoActual <= 0)
+        {
+            Debug.Log("Noche pasada");
+            tiempoActivo = false;
+        }
     }
+
+    private void CambiarTemporizador(bool estado)
+    {
+        tiempoActivo = estado;
+    }
+
+    public void ActivarTemporizador()
+    {
+        CambiarTemporizador(true);
+    }
+    public void DesactivarTemporizador()
+    {
+        CambiarTemporizador(false);
+    }
+
 }
